0a2ffbd0ed89a4f489b14020948a0c6106617141
Gustavo <gustavo.souza@lotsgroup.com>
2026-08-13 01:43:09 -0300

    Controle de Faltas - PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'controle-faltas-v2';
+const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', ev => {
+  ev.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQUIVOS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', ev => {
+  ev.waitUntil(
+    caches.keys()
+      .then(chaves => Promise.all(chaves.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', ev => {
+  if (ev.request.method !== 'GET') return;
+  ev.respondWith(
+    caches.match(ev.request, { ignoreSearch: true }).then(res =>
+      res ||
+      fetch(ev.request).then(resp => {
+        const copia = resp.clone();
+        caches.open(CACHE).then(c => c.put(ev.request, copia));
+        return resp;
+      })
+    )
+  );
+});
