48a254955c674aa13ff3604e19818eed05dde964
Gustavo <gustavo.souza@lotsgroup.com>
2026-08-13 04:35:32 -0300

    v2.0: 5 abas, notas PUC, ofensiva semanal, faltas limpa com gaveta, menu com conta/backup, agenda com descricao

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'faltae-v7';
+const CACHE = 'faltae-v8';
 const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', ev => {
