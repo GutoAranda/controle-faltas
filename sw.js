b8646a8b3a6c201cce8ae1a71029dae13ae44378
Gustavo <gustavo.souza@lotsgroup.com>
2026-08-13 02:56:35 -0300

    Faltae: rebrand + logo, notas e medias (quanto preciso tirar), onboarding, privacidade e termos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'controle-faltas-v5';
+const CACHE = 'faltae-v6';
 const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', ev => {
