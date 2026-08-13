d01ce34a09037724d332854366cfb8ecd34ddf22
Gustavo <gustavo.souza@lotsgroup.com>
2026-08-13 04:38:42 -0300

    Sequencia diaria na ofensiva

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'faltae-v8';
+const CACHE = 'faltae-v9';
 const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', ev => {
