551d6d6241a8ed5bbbc1516eb86cbcbbec112c2c
Gustavo <gustavo.souza@lotsgroup.com>
2026-08-13 02:32:29 -0300

    UX: atraso de conteudo via folha visual, falta na materia, aba persistente

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'controle-faltas-v4';
+const CACHE = 'controle-faltas-v5';
 const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', ev => {
