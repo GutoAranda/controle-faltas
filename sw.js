04fe008a9f0a344a3ef2a8c53eb7dc6162b4be57
Gustavo Aranda Freitas De Souza <gustavo.souza@lotsgroup.com>
2026-08-17 21:59:25 -0300

    Tela de criar conta proporcional: campos iguais (altura 45 e colunas minmax), cartao centralizado no desktop >=700px; sw v49

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'faltae-v48';
+const CACHE = 'faltae-v49';
 const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './catalogo-puc-2026-2.json'];
 
 self.addEventListener('install', ev => {
