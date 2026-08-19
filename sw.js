9cd86fdc349e54aa7a8ac5416bf58387eb4a1665
Gustavo Aranda Freitas De Souza <gustavo.souza@lotsgroup.com>
2026-08-19 10:36:56 -0300

    v68 tela do plano: chamada agora comunica VALOR e nao recurso ('O semestre inteiro sob controle - sem voce ter que controlar'), beneficios reordenados por forca de venda (widget > calendario > relatorio > materias ilimitadas > backup como garantia no fim) e tipografia calibrada pra 3 linhas nos 3 tamanhos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'faltae-v67';
+const CACHE = 'faltae-v68';
 const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './catalogo-puc-2026-2.json'];
 
 self.addEventListener('install', ev => {
