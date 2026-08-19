c098f1123e0b991da1e641d9267b18f195a0028e
Gustavo Aranda Freitas De Souza <gustavo.souza@lotsgroup.com>
2026-08-19 20:18:34 -0300

    v82 selecao de provas: todas as gavetas de materia nascem FECHADAS. A primeira vinha expandida, o que empurrava as outras materias pra fora da tela e escondia justamente a decisao que a tela pede (em qual materia entrar). Modo 'todas' continua abrindo, porque la os itens ja vem marcados e escondar isso seria pior

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'faltae-v81';
+const CACHE = 'faltae-v82';
 const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './catalogo-puc-2026-2.json'];
 
 self.addEventListener('install', ev => {
