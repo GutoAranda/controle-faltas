60377c2d3792d579a752db4ea614e60f392be769
Gustavo Aranda Freitas De Souza <gustavo.souza@lotsgroup.com>
2026-08-19 21:03:16 -0300

    v85 lembretes: parar de prometer aviso com o app fechado para aluno gratis. O push sai do servidor, que monta a mensagem lendo dados_usuario.dados na nuvem - e aluno gratis nao sincroniza, entao a nuvem dele nao tem prova alguma e o push nunca tem o que enviar. O app dizia 'voce recebe aviso de prova ate com o app fechado' para todo mundo: promessa falsa que so se revela no dia da prova que nao foi avisada. Agora o gratis le que o aviso aparece ao abrir o app, com o Essencial chegando no aparelho fechado - e ganha atalho para os planos. A inscricao do aparelho continua sendo gravada, para o push comecar a funcionar sozinho se ele assinar depois, sem pedir permissao de novo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'faltae-v84';
+const CACHE = 'faltae-v85';
 const ARQUIVOS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './catalogo-puc-2026-2.json'];
 
 self.addEventListener('install', ev => {
